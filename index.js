6743162ead5462462275a02d70cd707eab16d06a
KSH <kimsh@mida-ai.co.kr>
2021-07-20 00:04:10 +0900

    Todo 입력 만들기

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// 시간 보여주는 코드
+// 시간, Good ~ !! 보여주는 코드
 const todayTime = document.getElementById('todayTime');
 
 function clock() {
@@ -7,6 +7,18 @@ function clock() {
     const minutes = date.getMinutes();
 
     todayTime.innerText = `${hours} : ${minutes < 10 ? `0${minutes}` : minutes}`;
+    
+    function ment() {
+        let ment = document.getElementById('ment');
+
+        if(hours == '19', '20', '21', '22', '23', '0', '1', '2', '3', '4', '5', '6') {
+            ment.innerHTML = '<h3>Good Evening !!! </h3>';
+        } else if (hours == '7', '8', '9') {
+            ment.innerHTML = '<h3>Good Morning !!! </h3>';
+        }
+    }
+
+    ment();
 }
 
 function init() {
@@ -15,3 +27,26 @@ function init() {
 }
 
 init();
+
+const todoForm = document.getElementById('todo-form');
+const toDoInput = document.getElementById('todo-input');
+const todoList = document.getElementById('todo-list');
+
+function printToDo(createToDo) {
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+    span.innerText = createToDo.text;   
+    li.appendChild(span);
+    todoList.appendChild(li);
+}
+
+function submit(event)  {
+    event.preventDefault();
+    const newToDo = toDoInput.value;
+    const newTodoObj = {
+        text: newToDo
+    }
+    printToDo(newTodoObj);
+}
+
+todoForm.addEventListener("submit", submit);
