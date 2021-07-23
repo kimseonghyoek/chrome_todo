87f480be1e1839129ab0d7cd24257004bd4ca743
KSH <kimsh@mida-ai.co.kr>
2021-07-24 01:45:32 +0900

    delete todo list 수정할 것

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function clock() {
     todayTime.innerText = `${hours} : ${minutes < 10 ? `0${minutes}` : minutes}`;
     
     function ment() {
-        let ment = document.getElementById('ment');
+        const ment = document.getElementById('ment');
 
         if(hours == '19', '20', '21', '22', '23', '0', '1', '2', '3', '4', '5', '6') {
             ment.innerHTML = '<h3>Good Evening !!! </h3>';
@@ -34,22 +34,56 @@ const todoForm = document.getElementById('todo-form');
 const toDoInput = document.getElementById('todo-input');
 const todoList = document.getElementById('todo-list');
 
+let todos = [];
+
 function printToDo(createToDo) {
     const li = document.createElement('li');
     const span = document.createElement('span');
-    span.innerText = createToDo.text;   
+    const button = document.createElement('button');
+    button.innerText = '❌';
+    span.innerText = createToDo.text; 
     li.appendChild(span);
+    li.appendChild(button);
     todoList.appendChild(li);
+
+    button.onclick(delteTodo());
+}
+
+function delteTodo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+    todos = todos.filter((todo) => todo.id !== parseInt(li.id));
 }
 
 function submit(event)  {
     event.preventDefault();
+
     const newToDo = toDoInput.value;
+
     const newTodoObj = {
-        text: newToDo
+        text: newToDo,
+        id: Date.now()
     }
+    todos.push(newTodoObj);
+
+    if(toDoInput.value == '' || toDoInput.value == null) {
+        alert('빈 칸 인데...?');
+        return false;
+    }
+
     printToDo(newTodoObj);
     toDoInput.value = '';
+
+    function noEnter(e) {
+        if(todoList.childElementCount > 9) {
+            alert('10개 이상은 입력 금지입니다.');
+            toDoInput.disabled = true;
+        } else if(todoList.childElementCount < 9) {
+            toDoInput.disabled = false;
+        }
+    }
+    
+    noEnter();
 }
 
 todoForm.addEventListener("submit", submit);
